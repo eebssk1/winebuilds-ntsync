bdb2849bf0a722cb4cce8f8aad5b5df24e23956a
EBK21 <chkd13303@gmail.com>
2025-04-13 20:03:18 +0000

    update to 10.5

--- source.sh
+++ source.sh
@@ -2,12 +2,12 @@
 
 P=$PWD
 
-VER="wine-10.2"
+VER="wine-10.5"
 wget https://dl.winehq.org/wine/source/10.x/${VER}.tar.xz || exit 128
 tar --xz -xf ${VER}.tar.xz || exit 128
 rm -rf ${VER}.tar.xz
 
-git clone -b v10.2 https://github.com/wine-staging/wine-staging || exit 128
+git clone -b v10.5 https://github.com/wine-staging/wine-staging || exit 128
 pushd wine-staging
 ./staging/patchinstall.py DESTDIR="$P/${VER}" --all || exit 128
 popd
